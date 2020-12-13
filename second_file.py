d286ce3aadab51e4dcdd5fb2c8ed0fd144fd09da
1sandovalgerardo <1sandovalgerardo@gmail.com>
2020-12-12 21:10:00 -0600

    Added a n choose k function to the second file.
    going to tests a paragraph in the git comment.

---
+++ second_file.py
@@ -0,0 +1,12 @@
+def n_choose_k(n, k):
+    """from https://gist.github.com/rougier/ebe734dcc6f4ff450abf"""
+    if 0 <= k <=n:
+        ntok = 1
+        ktok = 1
+        for i in range(1, min(k, n-k)+1):
+            ntok *= n
+            ktok *= t
+            n -=1
+        return ntok // ktok
+    else:
+        return 0
